151a11aafe31b6da60042841e388ee68492abcad
DewanAlberts <dalberts@retrorabbit.co.za>
2023-05-04 08:23:44 +0200

    Add project files.

---
+++ WebApplication1/Models/RecommendationInputModelcs.cs
@@ -0,0 +1,17 @@
+﻿namespace Alberts_Dewan_Personal_Project.Models
+{
+    public class RecommendationInputModel
+
+    {
+        public string Province { get; set; }
+        public int EnergyBill { get; set; }
+        public int PeopleCount { get; set; }
+        public int HomeSize { get; set; }
+        public int WaterPumps { get; set; }
+        public int Aircons { get; set; }
+        public string StoveType { get; set; }
+        public int DryerCount { get; set; }
+        public int DishwasherCount { get; set; }
+        public int GeyserCount { get; set; }
+    }
+}
